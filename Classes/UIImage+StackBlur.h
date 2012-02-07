41c72183584b01c746684c6cdec510a1db6e25b9
tomsoft1 <thomas.landspurg@gmail.com>
2012-02-07 10:36:06 +0100

    Upadte README

--- Classes/UIImage+StackBlur.h
+++ Classes/UIImage+StackBlur.h
@@ -2,8 +2,11 @@
 //  UIImage+StackBlur.h
 //  stackBlur
 //
-//  Created by Thomas on 07/02/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//  Created by Thomas LANDSPURG on 07/02/12.
+//  Copyright 2012 Digiwie. All rights reserved.
+//
+// StackBlur implementation on iOS
+//
 //
 
 #import <Foundation/Foundation.h>
